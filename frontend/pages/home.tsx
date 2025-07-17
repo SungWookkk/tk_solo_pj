391fa3dcd4b334a2079daefcba892638bc6075d0
SungWookkk <patksy68@gmail.com>
2025-07-18 01:26:13 +0900

    7/18 feat: tailwind setting

---
+++ frontend/pages/home.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    // ① bg-green-200: 연한 초록 배경색
+    // ② text-green-800: 진한 초록 글자색
+    // ③ p-8: 안쪽 여백 padding: 2rem
+    // ④ rounded-lg: 테두리 둥글게
+    <div className="bg-green-200 text-green-800 p-8 rounded-lg">
+      Tailwind 적용 확인 ✅
+    </div>
+  )
+}
