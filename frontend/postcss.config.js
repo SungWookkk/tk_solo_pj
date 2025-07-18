75868ad5deaca45493d7ff3156602c191cd25bae
SungWookkk <patksy68@gmail.com>
2025-07-18 15:04:37 +0900

    7/18 feat: TailwindCSS NPM 문제점 해결 후 파일 추가

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},   // Tailwind를 PostCSS 플러그인으로 연결
-    autoprefixer: {},  // 벤더 접두사(-webkit-, -moz-) 자동 처리
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
   },
-};
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",      // Next.js app 폴더
-    "./pages/**/*.{js,ts,jsx,tsx}",    // pages 폴더
-    "./components/**/*.{js,ts,jsx,tsx}"// components 폴더
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {}, // 커스텀 테마를 여기에 추가
+    extend: {},
   },
-  plugins: [], // Tailwind 플러그인(예: forms)을 추가할 때 여기에
-};
+  plugins: [],
+}
